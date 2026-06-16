de65eb8f26a6e4bec9caafd8dcfd92ebd7c3b916
restoredelight <restoreddelight@proton.me>
2026-06-16 14:02:40 +0100

    move migrations to their own folder

--- shared/src/db.rs
+++ shared/src/db.rs
@@ -38,36 +38,6 @@ pub fn migrate(db: &rusqlite::Connection) -> Result<()> {
     Ok(())
 }
 
-const MIGRATIONS: [&str; 1] = [r#"
-    CREATE TABLE IF NOT EXISTS media (
-        id INTEGER PRIMARY KEY,
-        file_name TEXT NOT NULL,
-        file_type TEXT CHECK(file_type IN ('image','video','audio')) NOT NULL,
-        offset INTEGER,
-        length INTEGER,
-        path TEXT,
-        width INTEGER,
-        height INTEGER,
-        transparent INTEGER,
-        duration REAL,
-        audio INTEGER,
-        hash BLOB NOT NULL,
-        thumbnail BLOB
-    );
-    CREATE INDEX media_hash_index ON media (hash);
-    CREATE TABLE IF NOT EXISTS tags (
-        id INTEGER PRIMARY KEY,
-        name TEXT UNIQUE NOT NULL
-    );
-    CREATE TABLE IF NOT EXISTS media_tags (
-        media_id INTEGER NOT NULL,
-        tag_id INTEGER NOT NULL,
-        PRIMARY KEY(media_id, tag_id),
-        FOREIGN KEY(media_id) REFERENCES media(id) ON DELETE CASCADE,
-        FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
-    );
-    CREATE TABLE IF NOT EXISTS modes (
-        id INTEGER PRIMARY KEY,
-        file BLOB NOT NULL
-    );
-    "#];
+const MIGRATIONS: [&str; 1] = [
+    include_str!("migrations/0001_init_schema.sql")
+];
