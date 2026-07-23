060ac8fb26b9790549a2aecb5254e5c19457b4d1
restoredelight <restoreddelight@proton.me>
2026-07-23 17:58:18 +0100

    start the pack editor on a different port

--- pack-editor/vite.config.js
+++ pack-editor/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig(async () => ({
     fs: {
       allow: [".."],
     },
-    port: 1420,
+    port: 1421,
     strictPort: true,
     host: host || false,
     hmr: host
